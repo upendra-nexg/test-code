4b8544de87940fc830e1b0ca661b44c4b02be0ae
upendra-nexg <80741215+upendra-nexg@users.noreply.github.com>
2021-07-22 10:44:23 +0530

    Chnages some condition

--- services/handler.js
+++ services/handler.js
@@ -4,35 +4,41 @@ const postsTable = process.env.POSTS_TABLE;
 const docClient = new AWS.DynamoDB.DocumentClient({ apiVersion: '2012-08-10' });
 
 module.exports.getAllAudioInfo = async (event, context, callback) => {
-  if (event.queryStringParameters == null) {
-    let data = await getInfo().then(info => info).catch(err => console.log(err));
-    console.log(data);
-    let mainArr = [];
-    for (let i = 0; i < data.Count; i++) {
-      let mainObj = {
-        id:data.Items[i].id,
-        clientName:data.Items[i].clientName,
-        Info:data.Items[i].info
-      }
-      mainArr.push(mainObj);
+  let data = await getInfo().then(info => info).catch(err => console.log(err));
+  let mainArr = [];
+  for (let i = 0; i < data.Count; i++) {
+    let mainObj = {
+      id:data.Items[i].id,
+      clientName:data.Items[i].clientName,
+      Info:data.Items[i].info
     }
-    callback(null, { statusCode: 200, body: JSON.stringify(mainArr) });
-  } else {
-    let audioId = event.queryStringParameters;
-    let mainArr = [];
-    let audioInfo = await getAudioInfoById(audioId).then(info => info).catch(err => console.log(err));
-    if (audioInfo.Count > 0) {
+    mainArr.push(mainObj);
+  }
+  callback(null, { statusCode: 200, body: JSON.stringify(mainArr) });
+
+};
+
+module.exports.getAudioById = (event, context, callback) => {
+  let mainArr = [];
+  const params = {
+    TableName: 'Content_Assets',
+    Key: {
+      id:event.pathParameters.id,
+    },
+  };
+  docClient.get(params, (error, result) => {
+    if (result.Item != undefined) {
       let mainObj = {
-        id:audioInfo.Items[0].id,
-        clientName:audioInfo.Items[0].clientName,
-        Info:audioInfo.Items[0].info
+        id:result.Item.id,
+        clientName:result.Item.clientName,
+        Info:result.Item.info
       }
       mainArr.push(mainObj);
       callback(null, { statusCode: 200, body: JSON.stringify(mainArr) });
     } else {
       callback(null, { statusCode: 200, body: JSON.stringify('data not found!!') });
     }
-  }
+  });
 };
 
 
@@ -44,28 +50,6 @@ const getInfo = () => {
     docClient.scan(params, function (err, data) {
       if (err) {
         return reject(err);
-      } else {
-        return resolve(data);
-      }
-    });
-  });
-}
-
-const getAudioInfoById = (audioId) => {
-  return new Promise(async (resolve, reject) => {
-    const params = {
-      TableName: "Content_Assets",
-      KeyConditionExpression: "#id = :id",
-      ExpressionAttributeNames: {
-        "#id": "id"
-      },
-      ExpressionAttributeValues: {
-        ":id": audioId.id  // Sort key
-      }
-    };
-    docClient.query(params, function (err, data) {
-      if (err) {
-        return reject('data not fount');
       } else {
         return resolve(data);
       }
